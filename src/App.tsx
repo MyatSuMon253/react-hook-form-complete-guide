b6f94a3bb1c706ed36ae417948316130dc54b8af
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-09-25 20:55:30 +0630

    initial project setup with react-hook-form and nextui

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h3>Food Delivery App</h3>
+      </div>
+    </>
+  );
+}
+
+export default App;
