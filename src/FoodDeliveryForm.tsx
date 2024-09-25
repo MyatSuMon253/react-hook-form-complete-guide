e5b8b280b071bf8efba16c9864a1fcac6824f402
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-09-25 21:52:57 +0630

    feat: validate and submit form by using react hook form

--- src/FoodDeliveryForm.tsx
+++ src/FoodDeliveryForm.tsx
@@ -1,75 +1,36 @@
 import { Button, Input } from "@nextui-org/react";
-import { ChangeEvent, SyntheticEvent, useState } from "react";
+import { useForm } from "react-hook-form";
 
 type FoodDeliveryFormType = {
   customerName: string;
   mobile: string;
 };
 
-type FoodDeliveryFormErrorType = {
-  customerName: string;
-  mobile: string;
-};
-
 const FoodDeliveryForm = () => {
-  const [values, setValues] = useState<FoodDeliveryFormType>({
-    customerName: "",
-    mobile: "",
-  });
+  const { register, handleSubmit } = useForm<FoodDeliveryFormType>();
 
-  const [errors, setErrors] = useState<FoodDeliveryFormErrorType>({
-    customerName: "",
-    mobile: "",
-  });
-
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setValues({ ...values, [name]: value });
+  const onSubmit = (formData: FoodDeliveryFormType) => {
+    console.log(formData);
   };
 
-  const validateFormData = () => {
-    const newErrors: FoodDeliveryFormErrorType = {
-      customerName: "",
-      mobile: "",
-    };
-
-    if (values.customerName == "") {
-      newErrors.customerName = "Customer name is required.";
-    }
-    if (values.mobile == "") {
-      newErrors.mobile = "Mobile number is required.";
-    }
-    setErrors(newErrors);
-
-    return Object.values(errors).every((x) => x == "");
-  };
-
-  const onSubmit = (e: SyntheticEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    if (validateFormData()) {
-      console.log(values);
-    } else {
-      console.log("form is invalid");
-    }
+  const onError = (errors: any) => {
+    console.log(errors);
   };
 
   return (
-    <form autoComplete="false" onSubmit={onSubmit}>
+    <form autoComplete="false" onSubmit={handleSubmit(onSubmit, onError)}>
       <div>
         <Input
           type="text"
-          name="customerName"
           placeholder="Customer Name"
-          value={values.customerName}
-          onChange={handleInputChange}
+          {...register("customerName", {
+            required: "Customer name is required",
+          })}
         />
         <Input
           type="text"
-          name="mobile"
           placeholder="Mobile"
-          value={values.mobile}
-          onChange={handleInputChange}
+          {...register("mobile", { required: "Mobile number is required" })}
         />
         <Button type="submit">Submit</Button>
       </div>
---
+++ src/TypicalForm.tsx
@@ -0,0 +1,80 @@
+import { Button, Input } from "@nextui-org/react";
+import { ChangeEvent, SyntheticEvent, useState } from "react";
+
+type FoodDeliveryFormType = {
+  customerName: string;
+  mobile: string;
+};
+
+type FoodDeliveryFormErrorType = {
+  customerName: string;
+  mobile: string;
+};
+
+const FoodDeliveryForm = () => {
+  const [values, setValues] = useState<FoodDeliveryFormType>({
+    customerName: "",
+    mobile: "",
+  });
+
+  const [errors, setErrors] = useState<FoodDeliveryFormErrorType>({
+    customerName: "",
+    mobile: "",
+  });
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  const validateFormData = () => {
+    const newErrors: FoodDeliveryFormErrorType = {
+      customerName: "",
+      mobile: "",
+    };
+
+    if (values.customerName == "") {
+      newErrors.customerName = "Customer name is required.";
+    }
+    if (values.mobile == "") {
+      newErrors.mobile = "Mobile number is required.";
+    }
+    setErrors(newErrors);
+
+    return Object.values(errors).every((x) => x == "");
+  };
+
+  const onSubmit = (e: SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (validateFormData()) {
+      console.log(values);
+    } else {
+      console.log("form is invalid");
+    }
+  };
+
+  return (
+    <form autoComplete="false" onSubmit={onSubmit}>
+      <div>
+        <Input
+          type="text"
+          name="customerName"
+          placeholder="Customer Name"
+          value={values.customerName}
+          onChange={handleInputChange}
+        />
+        <Input
+          type="text"
+          name="mobile"
+          placeholder="Mobile"
+          value={values.mobile}
+          onChange={handleInputChange}
+        />
+        <Button type="submit">Submit</Button>
+      </div>
+    </form>
+  );
+};
+
+export default FoodDeliveryForm;
