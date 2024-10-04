c14f1adc2fa2b6ed718624e7f2e6a5cf2efd7674
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-10-04 20:39:40 +0630

    chore: add survey form ui draft

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,54 @@
-import "./App.css";
-import FoodDeliveryForm from "./pages/FoodDelivery/FoodDeliveryForm";
+import { useForm } from "react-hook-form";
 
-function App() {
+type FormData = {
+  reason: string;
+  otherReason: string;
+};
+
+const App = () => {
+  const methods = useForm<FormData>({
+    mode: "onChange",
+    defaultValues: {
+      reason: "",
+      otherReason: "",
+    },
+  });
+
+  const { register, watch, handleSubmit } = methods;
+
+  const onSubmit = (formData: FormData) => {
+    console.log(formData);
+  };
+
   return (
-    <>
-      <div>
-        <h3>Food Delivery App</h3>
-        {/* <TypicalForm /> */}
-        <FoodDeliveryForm />
-      </div>
-    </>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <h3>How did you found us? </h3>
+      <label>
+        <input type="radio" value="friend" {...register("reason")} />
+        Through a friend
+      </label>
+      <label>
+        <input type="radio" value="social_media" {...register("reason")} />
+        Through social media
+      </label>
+      <label>
+        <input type="radio" value="ads" {...register("reason")} />
+        Through our ads
+      </label>
+      <label>
+        <input type="radio" value="other" {...register("reason")} />
+        Other reason
+      </label>
+      <br />
+      {watch("reason") === "other" && (
+        <>
+          <input placeholder="Please describe" {...register("otherReason")} />
+          <br />
+        </>
+      )}
+      <input type="submit" value="Submit" />
+    </form>
   );
-}
+};
 
 export default App;
---
+++ src/FoodDeliveryApp.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import FoodDeliveryForm from "./pages/FoodDelivery/FoodDeliveryForm";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h3>Food Delivery App</h3>
+        {/* <TypicalForm /> */}
+        <FoodDeliveryForm />
+      </div>
+    </>
+  );
+}
+
+export default App;
